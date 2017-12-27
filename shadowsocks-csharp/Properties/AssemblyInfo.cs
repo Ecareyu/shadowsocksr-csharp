6d69d7e907a39a44e610bb0e77066a0b410fe43a
ecareyu <ecareyu@gmail.com>
2017-12-27 22:53:08 +0800

    fix .net 4.0 error add description to about form

--- shadowsocks-csharp/Properties/AssemblyInfo.cs
+++ shadowsocks-csharp/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些属性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle(UpdateChecker.Name)]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(UpdateChecker.Description)]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct(UpdateChecker.Name)]
